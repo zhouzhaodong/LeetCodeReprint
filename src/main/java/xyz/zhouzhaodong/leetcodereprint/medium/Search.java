e3904375852a225d3538fce4c18144a718cdcbdb
周兆东 <xiuaiba@163.com>
2020-04-27 16:17:56 +0800

    中等题 5.搜索旋转排序数组

---
+++ src/main/java/xyz/zhouzhaodong/leetcodereprint/medium/Search.java
@@ -0,0 +1,38 @@
+package xyz.zhouzhaodong.leetcodereprint.medium;
+
+/**
+ * 33. 搜索旋转排序数组
+ *
+ * @author 84266
+ */
+public class Search {
+
+    /**
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+     * 你可以假设数组中不存在重复的元素。
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search(int[] nums, int target) {
+        // 暴力循环法，也可以使用二分法
+        if (nums.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+
+}
